afd88fa160f2b259e4613cfa37f08cd06db9629a
ArianSa89 <ariansalehith89th@gmail.com>
2026-09-12 21:12:45 +0330

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
 KEYWORD_REPLIES = {
     "سلام": "علیک",
     "گوه بات": "دلم جون",
-    " فعلا عشق"" خدافظ",
+    "خدافظ": "فعلا",
     "مرسی": "فدا",
     "مرسی زیاد": "عشق",
     "کمک": "@ArianSa89.",
